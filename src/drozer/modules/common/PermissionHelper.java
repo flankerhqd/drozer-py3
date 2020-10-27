f3586aff85aae83d444fdbc16cae87d959396d13
LeadroyaL <243850435@qq.com>
2020-10-27 19:50:34 +0800

    [bug] PermissionHelper now list all permissions.

--- src/drozer/modules/common/PermissionHelper.java
+++ src/drozer/modules/common/PermissionHelper.java
@@ -1,5 +1,6 @@
 import android.content.pm.PackageManager;
 import android.content.pm.PermissionInfo;
+import android.content.pm.PermissionGroupInfo;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -7,12 +8,24 @@ class PermissionHelper{
     public static String query(PackageManager pm, String permissionGroup, int flags) {
         try {
             JSONArray jsonArray = new JSONArray();
-            for (PermissionInfo info : pm.queryPermissionsByGroup(null, 0)) {
+            for (PermissionInfo info : pm.queryPermissionsByGroup(permissionGroup, flags)) {
                 JSONObject jsonObject = new JSONObject();
                 jsonObject.put("packageName", info.packageName);
                 jsonObject.put("name", info.name);
                 jsonObject.put("protectionLevel", info.protectionLevel);
                 jsonArray.put(jsonObject);
+            }
+            if (permissionGroup == null) {
+                // append all groups except `null` group
+                for (PermissionGroupInfo groupInfo : pm.getAllPermissionGroups(0)) {
+                    for (PermissionInfo info : pm.queryPermissionsByGroup(groupInfo.name, flags)) {
+                        JSONObject jsonObject = new JSONObject();
+                        jsonObject.put("packageName", info.packageName);
+                        jsonObject.put("name", info.name);
+                        jsonObject.put("protectionLevel", info.protectionLevel);
+                        jsonArray.put(jsonObject);
+                    }
+                }
             }
             return jsonArray.toString();
         } catch (Exception e) {
